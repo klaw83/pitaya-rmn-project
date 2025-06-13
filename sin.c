18c89e342014d9a155b0574381b1c70deeb32572
Page <adrien.page.0@gmail.com>
2025-06-13 15:53:14 +0200

    une seule sortie

--- sin.c
+++ sin.c
@@ -33,23 +33,13 @@ int main(int argc, char **argv){
     rp_GenReset();
 
     /* Generation */
-    rp_GenSynchronise();
-
     rp_GenWaveform(RP_CH_1, RP_WAVEFORM_ARBITRARY);
-    rp_GenWaveform(RP_CH_2, RP_WAVEFORM_ARBITRARY);
 
     rp_GenArbWaveform(RP_CH_1, x, buff_size);
-    rp_GenArbWaveform(RP_CH_2, y, buff_size);
 
     rp_GenAmp(RP_CH_1, 0.7);
-    rp_GenAmp(RP_CH_2, 1.0);
 
     rp_GenFreq(RP_CH_1, 4000.0);
-    rp_GenFreq(RP_CH_2, 4000.0);
-
-    rp_GenOutEnableSync(True)
-
-    rp_GenSynchronise()
 
     /* Releasing resources */
     free(y);
