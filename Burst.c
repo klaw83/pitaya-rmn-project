a69297b09f0bf871f64f81af4605ce790052bd37
Page <adrien.page.0@gmail.com>
2025-06-13 15:48:14 +0200

    Qu'une seule sortie burst

--- Burst.c
+++ Burst.c
@@ -20,27 +20,14 @@ int main(int argc, char **argv){
     rp_GenFreq(RP_CH_1, 4);
     rp_GenAmp(RP_CH_1, 1.0);
 
-    rp_GenWaveform(RP_CH_2, RP_WAVEFORM_SINE);
-    rp_GenFreq(RP_CH_2, 4);
-    rp_GenAmp(RP_CH_2, 1.0);
-
     rp_GenMode(RP_CH_1, RP_GEN_MODE_BURST);
     rp_GenBurstCount(RP_CH_1, 2);
     rp_GenBurstRepetitions(RP_CH_1, 1);
     rp_GenBurstPeriod(RP_CH_1, 5000);
 
-    rp_GenMode(RP_CH_2, RP_GEN_MODE_BURST);
-    rp_GenBurstCount(RP_CH_2, 2);
-    rp_GenBurstRepetitions(RP_CH_2, 1);
-    rp_GenBurstPeriod(RP_CH_2, 5000);
-
-    rp_GenOutEnableSync(true);
     sleep(2);
+    
     rp_GenTriggerOnly(RP_CH_1);
-    sleep(2);
-    rp_GenTriggerOnly(RP_CH_2);
-    sleep(1);
-    rp_GenSynchronise();
-
+    
     rp_Release();
 }
