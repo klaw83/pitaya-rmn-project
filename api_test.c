1ff6a595cacb7e885fae19c6f68aac0479d9bdc3
Page <adrien.page.0@gmail.com>
2025-06-16 13:59:37 +0200

    Ajout de deux fichiers

---
+++ api_test.c
@@ -0,0 +1,9 @@
+/* Red Pitaya C API example Acquiring a signal from a buffer
+ * This application acquires a signal on a specific channel */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "rp.h"
+
+#define DATA_SIZE 64
+
