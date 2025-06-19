a02b4e7958165db3bbe7e6e65095f87e2f6b6150
Page <adrien.page.0@gmail.com>
2025-06-19 10:49:15 +0200

    decimation at 1

--- Aquisition_axi.c
+++ Aquisition_axi.c
@@ -11,7 +11,7 @@
 int main(int argc, char **argv)
 {
     int dsize = DATA_SIZE;
-    uint32_t dec = 8192;
+    uint32_t dec = 1;
     if (argc >= 3){
         dsize = atoi(argv[1]);
         dec = atoi(argv[2]);
