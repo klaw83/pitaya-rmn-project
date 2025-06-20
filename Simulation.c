0dc3967a4f837953c6945bb1550eca496fc66a16
Page <adrien.page.0@gmail.com>
2025-06-20 14:27:57 +0200

    test trigger

--- Simulation.c
+++ Simulation.c
@@ -2,3 +2,55 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include "rp.h"
+
+int main (int argc, char **argv) {
+    int unsigned period = 1000000; // uS
+    int unsigned led;
+
+
+    if (argc > 1) {
+        led = atoi(argv[1]);
+    }
+
+    printf("Blinking LED[0]\n");
+    printf("Emulation started\n");
+
+    led = RP_LED0;
+
+    // Initialization of API
+    if (rp_Init() != RP_OK) {
+        fprintf(stderr, "Red Pitaya API init failed!\n");
+        return EXIT_FAILURE;
+    }
+
+    rp_AcqSetTriggerLevel(RP_T_CH_1,0);
+
+    if (rp_AcqStart() != RP_OK) {
+        fprintf(stderr, "rp_AcqStart failed!\n");
+        return -1;
+    }
+
+    rp_AcqSetTriggerSrc(RP_TRIG_SRC_CHA_PE);
+    rp_acq_trig_state_t state = RP_TRIG_STATE_TRIGGERED;
+
+    int unsigned retries = 120; //durée de la simulation en s
+    while (retries--){
+        //led indiquant que la simulation tourne
+        rp_DpinSetState(led, RP_HIGH);
+        usleep(period/2);
+        rp_DpinSetState(led, RP_LOW);
+        usleep(period/2);
+    
+        rp_AcqGetTriggerState(&state);
+        if(state == RP_TRIG_STATE_TRIGGERED){
+            sleep(1);
+            break;
+        }
+    }
+    
+    rp_DpinSetState(led, RP_LOW);
+    // Releasing resources
+    rp_Release();
+
+    return EXIT_SUCCESS;
+}
