6407f32b93ad4bbe80717244729812e1e917e5dc
Page <adrien.page.0@gmail.com>
2025-06-20 14:59:01 +0200

    burst reglag

--- Simulation.c
+++ Simulation.c
@@ -64,7 +64,7 @@ int main (int argc, char **argv) {
 
     rp_GenMode(RP_CH_1, RP_GEN_MODE_BURST);
     rp_GenBurstCount(RP_CH_1, 1);
-    rp_GenBurstRepetitions(RP_CH_1, 10000);
+    rp_GenBurstRepetitions(RP_CH_1, 10);
     rp_GenBurstPeriod(RP_CH_1, 5000);
 
     rp_GenOutEnable(RP_CH_1);
