875ff2f4dc32755fd7626d9c895b64fc8b15c23a
Page <adrien.page.0@gmail.com>
2025-06-20 15:58:49 +0200

    aml delay

--- Simulation.c
+++ Simulation.c
@@ -54,14 +54,14 @@ int main (int argc, char **argv) {
         if(state == RP_TRIG_STATE_TRIGGERED){
             break;
         }
-            
+        /*     
         //led indiquant que la simulation tourne
         rp_DpinSetState(led, RP_HIGH);
         rp_DpinSetState(led+1, RP_LOW);
         usleep(period/2);
         rp_DpinSetState(led, RP_LOW);
         rp_DpinSetState(led+1, RP_HIGH);
-        usleep(period/2);
+        usleep(period/2); */
     }
 
     //déclenchement out1 NOW
