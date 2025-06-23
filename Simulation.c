47559cc3b4ba19de31ba554a114c60e30aaea729
Page <adrien.page.0@gmail.com>
2025-06-23 15:38:48 +0200

    sine

--- Simulation.c
+++ Simulation.c
@@ -39,7 +39,7 @@ int main (int argc, char **argv) {
     //Activation du port out1
     rp_GenReset();
 
-    rp_GenWaveform(RP_CH_1, SINE);
+    rp_GenWaveform(RP_CH_1, RP_WAVEFORM_SINE);
     rp_GenFreq(RP_CH_1, 1000);
     rp_GenAmp(RP_CH_1, 2.0);
 
