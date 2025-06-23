8cdb5ec35dd79deca40b9878f6f1c4bae9353293
Page <adrien.page.0@gmail.com>
2025-06-23 16:12:44 +0200

    changed with trig now

--- old_Aquisition.c
+++ old_Aquisition.c
@@ -64,7 +64,7 @@ int main(int argc, char **argv)
         return -1;
     }
 
-    rp_AcqSetTriggerSrc(RP_TRIG_SRC_CHA_PE);
+    rp_AcqSetTriggerSrc(RP_TRIG_SRC_NOW);
     rp_acq_trig_state_t state = RP_TRIG_STATE_TRIGGERED;
 
     while(1){
