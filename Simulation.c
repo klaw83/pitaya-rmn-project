207867d297ee1da95a82f4447e85cf996796047b
Page <adrien.page.0@gmail.com>
2025-06-23 10:05:14 +0200

    led pour plus rapide ?

--- Simulation.c
+++ Simulation.c
@@ -54,7 +54,6 @@ int main (int argc, char **argv) {
         if(state == RP_TRIG_STATE_TRIGGERED){
             printf("trigger\n");
             rp_GenOutEnable(RP_CH_1);
-    
             //déclenchement out1 NOW
             rp_GenTriggerOnly(RP_CH_1);
             break;
@@ -63,17 +62,9 @@ int main (int argc, char **argv) {
              
         //led indiquant que la simulation tourne
         rp_DpinSetState(led, RP_HIGH);
-        rp_DpinSetState(led+1, RP_LOW);
-        usleep(period/2);
-        rp_DpinSetState(led, RP_LOW);
-        rp_DpinSetState(led+1, RP_HIGH);
-        usleep(period/2);
     }
 
-    
-
     rp_DpinSetState(led, RP_LOW);
-    rp_DpinSetState(led+1, RP_LOW);
     // Releasing resources
     rp_Release();
 
