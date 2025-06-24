1bf291f138468ed2919958ec1ce438b1d8c334d5
Page <adrien.page.0@gmail.com>
2025-06-24 16:07:44 +0200

    repetition 1000

--- Simulation.c
+++ Simulation.c
@@ -49,7 +49,7 @@ int main (int argc, char **argv) {
 
     rp_GenMode(RP_CH_1, RP_GEN_MODE_BURST);
     rp_GenBurstCount(RP_CH_1, number_burst_cycle);
-    rp_GenBurstRepetitions(RP_CH_1, 1);
+    rp_GenBurstRepetitions(RP_CH_1, 1000);
     rp_GenBurstPeriod(RP_CH_1, 1);
     
     //Attente
