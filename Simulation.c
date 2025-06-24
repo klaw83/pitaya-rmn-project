b0ee026d5dc415f90f1382a69350a78e87717c5f
Page <adrien.page.0@gmail.com>
2025-06-24 16:35:15 +0200

    3500

--- Simulation.c
+++ Simulation.c
@@ -50,7 +50,7 @@ int main (int argc, char **argv) {
     rp_GenAmp(RP_CH_1, answer_amplitude_Volts);
 
     rp_GenMode(RP_CH_1, RP_GEN_MODE_BURST);
-    rp_GenBurstCount(RP_CH_1, 35000000);
+    rp_GenBurstCount(RP_CH_1, 3500);
     rp_GenBurstRepetitions(RP_CH_1, 1);
     rp_GenBurstPeriod(RP_CH_1, 1000);
     
