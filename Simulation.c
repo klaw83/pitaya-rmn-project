c7e79cc009113586f276d8afe57e8f3540cf674d
Page <adrien.page.0@gmail.com>
2025-06-25 09:51:00 +0200

    Les periodes ne sont pas collés quand on augmente la fréquence test avec 65536 periodes

--- Simulation.c
+++ Simulation.c
@@ -61,8 +61,8 @@ int main (int argc, char **argv) {
     rp_GenAmp(RP_CH_1, 2.0);
 
     rp_GenMode(RP_CH_1, RP_GEN_MODE_BURST);
-    rp_GenBurstCount(RP_CH_1, Larmor_frequency_Hertz);
-    rp_GenBurstRepetitions(RP_CH_1, 1000);
+    rp_GenBurstCount(RP_CH_1, 65536);
+    //rp_GenBurstRepetitions(RP_CH_1, 1000);
     rp_GenBurstPeriod(RP_CH_1, 1);
     
     //Attente
