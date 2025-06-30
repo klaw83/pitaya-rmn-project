3fb115ec1f0fc779b78f015255fa089424c512cd
Page <adrien.page.0@gmail.com>
2025-06-30 15:17:38 +0200

    ok peut etre

--- Aquisition_axi.c
+++ Aquisition_axi.c
@@ -19,7 +19,7 @@ int main(int argc, char **argv)
     uint32_t posChA;
     bool fillState = false;
     
-    float excitation_duration_seconds = 15.027e-06; //41.027e-06
+    float excitation_duration_seconds = 50.708e-06; //41.027e-06
     float excitation_duration_microseconds = excitation_duration_seconds*1000000;
     float excitation_amplitude_Volts = 0.19;
     float Larmor_frequency_Hertz = 24378040.422;
