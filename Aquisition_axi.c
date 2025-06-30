38047ec0e0e0b7252303c283904cd97dc1a7c139
Page <adrien.page.0@gmail.com>
2025-06-30 16:11:19 +0200

    valeurs

--- Aquisition_axi.c
+++ Aquisition_axi.c
@@ -19,10 +19,10 @@ int main(int argc, char **argv)
     uint32_t posChA;
     bool fillState = false;
     
-    float excitation_duration_seconds = 45.384e-06; //41.027e-06
+    float excitation_duration_seconds = 45.493e-06; //41.027e-06
     float excitation_duration_microseconds = excitation_duration_seconds*1000000;
     float excitation_amplitude_Volts = 0.19;
-    float Larmor_frequency_Hertz = 24351186.267;
+    float Larmor_frequency_Hertz = 24351298,957;
     int excitation_burst_cycles_tot = Larmor_frequency_Hertz *excitation_duration_seconds;
     float oscillator_frequency = Larmor_frequency_Hertz + 1000;
     float oscillator_amplitude_Volts = 0.8;
