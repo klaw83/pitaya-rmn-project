aa419f7ae2daf5144790c5f7b297750935d3b8b6
page-icube <33966313+klaw83@users.noreply.github.com>
2025-08-28 10:43:05 +0200

    Merge Pitaya tests repo and clean

--- old_programs/api_test.c
+++
@@ -1,9 +0,0 @@
-/* Red Pitaya C API example Acquiring a signal from a buffer
- * This application acquires a signal on a specific channel */
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include "rp.h"
-
-#define DATA_SIZE 64
-
