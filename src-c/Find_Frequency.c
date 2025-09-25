2fc90612dd04c44346097a07a9369738ed3a6156
page-icube <33966313+klaw83@users.noreply.github.com>
2025-09-25 13:58:40 +0200

    Première version clean avec affichage marche avec le pure device

--- src-c/Find_Frequency.c
+++ src-c/Find_Frequency.c
@@ -21,14 +21,14 @@
  * @param fichier           Pointeur vers le fichier ouvert en écriture.
  * @param dsize             Taille des données à enregistrer.
  * @param dec               Facteur de décimation utilisé lors de l'acquisition.
- * @param number_of_files   Nombre de fichiers à traiter/enregistrer.
+ * @param number_of_steps   Nombre de fichiers à traiter/enregistrer.
  *
  * @return int              0 si succès, -1 si erreur lors de la récupération du gain.
  *
  * @note Le offset est actuellement fixé à 0 car la fonction rp_AcqAxiGetOffset n'est pas supportée.
  * @note Le gain est récupéré via rp_AcqGetGainV sur le canal 2 (RP_CH_2).
  */
-int create_file(FILE* fichier, int dsize, int dec, int number_of_files){   
+int create_file(FILE* fichier, int dsize, int dec, int number_of_steps){   
 
     float gainValue; //1 ou 20V
     
@@ -47,13 +47,31 @@ int create_file(FILE* fichier, int dsize, int dec, int number_of_files){
 
     fprintf(fichier, "%d, ", dsize );
     fprintf(fichier, "%d, ", dec );
-    fprintf(fichier, "%d, ", number_of_files);
+    fprintf(fichier, "%d, ", number_of_steps);
     fprintf(fichier, "%f, ",(float) gainValue);
     fprintf(fichier, "%f, ", offset);
     fprintf(fichier, "%d\n", nb_bits);
 
     return 0;
 }
+
+float find_max(float buff[], int size) {
+    if (size <= 0) {
+        printf("Error: Array size must be positive\n");
+        return -1; // or handle error appropriately
+    }
+    
+    float max = abs(buff[0]); // Initialize max with first element
+    
+    for (int i = 1; i < size; i++) {
+        if (abs(buff[i]) > max) {
+            max = abs(buff[i]);
+        }
+    }
+    
+    return max;
+}
+
 
 int main(int argc, char **argv)
 {
@@ -66,33 +84,51 @@ int main(int argc, char **argv)
     float Larmor_frequency_Hertz = 24351392.574;
     float oscillator_amplitude_Volts = 0.8;
 
-    int number_of_files = 1;
+    int number_of_steps = 1;
     char nomFichier[256];
     rp_pinState_t Gain = RP_HIGH;
+    rp_channel_t CH_ACQ = RP_CH_1;
+
 
     rp_acq_trig_state_t state;
     uint32_t g_adc_axi_start,g_adc_axi_size;
 
-    //// ARGUMENTS DU PROGRAMME /////
+    ////-------- ARGUMENTS DU PROGRAMME
     dsize = atoi(argv[1]);
     dec = atoi(argv[2]);
-    number_of_files = atoi(argv[3]);
+    number_of_steps = atoi(argv[3]);    
     strcpy(nomFichier, argv[4]);
     Larmor_frequency_Hertz = atof(argv[5]);
     excitation_duration_seconds = atof(argv[6]);
     float step_frequency_Hertz = atof(argv[7]);
     int wait = atoi(argv[8]);
-    
-    printf("larmor %f, duration excitation %f\n",Larmor_frequency_Hertz, excitation_duration_seconds);
+    ////-------- ----------------------
+
+
+    printf("original Larmor frequency %f, constant duration excitation %f\n",Larmor_frequency_Hertz, excitation_duration_seconds);
 
     int excitation_burst_cycles_tot = Larmor_frequency_Hertz * excitation_duration_seconds;
+    printf("Initial excitation burst cycles = %d\n \n",excitation_burst_cycles_tot);
+
     float oscillator_frequency = Larmor_frequency_Hertz + 1000;
-    
+    float excitation_duration_useconds = excitation_duration_seconds*1000000;
 
     float *buff1 = (float *)malloc(dsize * sizeof(float));
     uint32_t posChA;
     bool fillState = false;
+
+
+    ////-------- Table initialisation
+    float *maximum_amplitude_t = (float*) malloc((number_of_steps+2) * sizeof(float));
+    float *Larmor_frequency_Hertz_t = (float*) malloc((number_of_steps+2) * sizeof(float));
+
+    maximum_amplitude_t[0] = 0; //Initial value of the max
+
+    Larmor_frequency_Hertz_t[0] = Larmor_frequency_Hertz;
     
+    Larmor_frequency_Hertz_t[1] = Larmor_frequency_Hertz;
+
+    ////---------------------------
 
     if (rp_InitReset(false) != RP_OK) {
             fprintf(stderr, "Rp api init failed!\n");
@@ -109,11 +145,11 @@ int main(int argc, char **argv)
         fprintf(stderr, "rp_AcqAxiSetDecimationFactor failed!\n");
         return -1;
     }
-    if (rp_AcqAxiSetTriggerDelay(RP_CH_2, dsize)  != RP_OK) { 
-        fprintf(stderr, "rp_AcqAxiSetTriggerDelay RP_CH_2 failed!\n");
+    if (rp_AcqAxiSetTriggerDelay(CH_ACQ, dsize)  != RP_OK) { 
+        fprintf(stderr, "rp_AcqAxiSetTriggerDelay CH_ACQ failed!\n");
         return -1;
     }
-    if (rp_AcqSetGain(RP_CH_2, Gain) != RP_OK){
+    if (rp_AcqSetGain(CH_ACQ, Gain) != RP_OK){
         fprintf(stderr, "rp_AcqSetGain CH1 Failed\n");
         return -1;
     }
@@ -140,11 +176,7 @@ int main(int argc, char **argv)
     if(rp_GenMode(RP_CH_1, RP_GEN_MODE_BURST) != RP_OK){
         fprintf(stderr, "rp_GenMode RP_CH_1 BURST failed!\n");
         return -1;
-    }    
-    if(rp_GenBurstCount(RP_CH_1, excitation_burst_cycles_tot) != RP_OK){
-        fprintf(stderr, "rp_GenBurstCount RP_CH_1 failed!\n");
-        return -1;
-    }//valeur max pour GenBurstCount = 50 000
+    }
     if(rp_GenBurstRepetitions(RP_CH_1, 1) != RP_OK){
         fprintf(stderr, "rp_GenBurstRepetitions RP_CH_1 failed!\n");
         return -1;
@@ -161,28 +193,28 @@ int main(int argc, char **argv)
 
     //// INITIALISATION ET LANCEMENT DE L'OSCILLATEUR LOCAL
     if(rp_GenWaveform(RP_CH_2, RP_WAVEFORM_SINE) != RP_OK){
-        fprintf(stderr, "rp_GenWaveform RP_CH_1 SINE failed!\n");
+        fprintf(stderr, "rp_GenWaveform RP_CH_2 SINE failed!\n");
         return -1;
     }
     if(rp_GenFreq(RP_CH_2, oscillator_frequency) != RP_OK){
-        fprintf(stderr, "rp_GenFreq RP_CH_1 failed!\n");
+        fprintf(stderr, "rp_GenFreq RP_CH_2 failed!\n");
         return -1;
     }
     if(rp_GenAmp(RP_CH_2, oscillator_amplitude_Volts) != RP_OK){
-        fprintf(stderr, "rp_GenAmp RP_CH_1 failed!\n");
+        fprintf(stderr, "rp_GenAmp RP_CH_2 failed!\n");
         return -1;
     }
     if(rp_GenMode(RP_CH_2, RP_GEN_MODE_CONTINUOUS) != RP_OK){
-        fprintf(stderr, "rp_GenMode RP_CH_1 BURST failed!\n");
+        fprintf(stderr, "rp_GenMode RP_CH_2 BURST failed!\n");
         return -1;
     }
     if( rp_GenOutEnable(RP_CH_2) != RP_OK){
-        fprintf(stderr, "rp_GenOutEnable RP_CH_1 failed!\n");
+        fprintf(stderr, "rp_GenOutEnable RP_CH_2 failed!\n");
         return -1;
     }
   
 
-    //// CREATION DU FICHIER
+    //// CREATION DU FICHIER    
     FILE *fichier = fopen(nomFichier, "w");
     printf("fichier crée : ");
     puts(nomFichier);
@@ -190,38 +222,33 @@ int main(int argc, char **argv)
         perror("Erreur lors de l'ouverture du fichier\n");
         return -1; // Quitte le programme avec erreur
     }
-    if (create_file(fichier, dsize, dec, number_of_files)){
+    if (create_file(fichier, dsize, dec, number_of_steps)){
         perror("Erreur de creation fichier");
         return -1;
     }
+
 
     
     //////////BOUCLE DE FICHIERS//////////
     rp_DpinSetState(RP_LED0+1, RP_HIGH);
     clock_t begin = clock();
     int i=0;
-    for (i=0;i<number_of_files;i++){
+    for (i=0;i<number_of_steps;i++){
 
-        /////// CHANGEMENT DU TEMPS DEXCITATION //////
-        printf("Balayage de 100us a 200us, pas de 10us\n");
-        Larmor_frequency_Hertz = Larmor_frequency_Hertz + step_frequency_Hertz*i;
-        excitation_burst_cycles_tot = Larmor_frequency_Hertz *excitation_duration_seconds;
-        printf("frequence de larmor %f\n",Larmor_frequency_Hertz);
-        printf("nombre des periodes dans le burst %d\n",excitation_burst_cycles_tot);
 
         if(rp_GenBurstCount(RP_CH_1, excitation_burst_cycles_tot) != RP_OK){
             fprintf(stderr, "rp_GenBurstCount RP_CH_1 failed!\n");
             return -1;
         }//valeur max pour GenBurstCount = 50 000
 
-        if(rp_GenFreq(RP_CH_1, Larmor_frequency_Hertz) != RP_OK){
+        if(rp_GenFreq(RP_CH_1, *Larmor_frequency_Hertz_t) != RP_OK){
             fprintf(stderr, "rp_GenFreq RP_CH_1 failed!\n");
         return -1;
         }
 
-        oscillator_frequency = Larmor_frequency_Hertz + 1000;
+        oscillator_frequency = *Larmor_frequency_Hertz_t + 1000;
         if(rp_GenFreq(RP_CH_2, oscillator_frequency) != RP_OK){
-            fprintf(stderr, "rp_GenFreq RP_CH_1 failed!\n");
+            fprintf(stderr, "rp_GenFreq RP_CH_2 failed!\n");
         return -1;
         }
         
@@ -231,21 +258,28 @@ int main(int argc, char **argv)
         rp_AcqAxiGetMemoryRegion(&g_adc_axi_start,&g_adc_axi_size);
         //printf("Reserved memory start 0x%X size 0x%X bytes\n",g_adc_axi_start,g_adc_axi_size);
 
-        if (rp_AcqAxiSetBufferSamples(RP_CH_2,g_adc_axi_start, dsize) != RP_OK) {
-        fprintf(stderr, "rp_AcqAxiSetBuffer RP_CH_2 failed!\n");
+        if (rp_AcqAxiSetBufferSamples(CH_ACQ,g_adc_axi_start, dsize) != RP_OK) {
+        fprintf(stderr, "rp_AcqAxiSetBuffer CH_ACQ failed!\n");
         return -1;
         }
-        if (rp_AcqAxiEnable(RP_CH_2, true)) {
-            fprintf(stderr, "rp_AcqAxiEnable RP_CH_2 failed!\n");
+        if (rp_AcqAxiEnable(CH_ACQ, true)) {
+            fprintf(stderr, "rp_AcqAxiEnable CH_ACQ failed!\n");
             return -1;
         }
 
-        //LANCEMENT, DECLENCGEMENT DE L'AQUISITION AVANT LE BURST
+       ////////////DECLENCHEMENT SYNCHRONISE///////////////
+        if(rp_GenSynchronise() != RP_OK){
+            fprintf(stderr, "rp_GenSynchronise failed!\n");
+            return -1;
+        }
+
+        usleep(excitation_duration_useconds);
+        //LANCEMENT, DECLENCGEMENT DE L'AQUISITION 
         if (rp_AcqStart() != RP_OK) {
         fprintf(stderr, "rp_AcqStart failed!\n");
         return -1;
         }
-        if( rp_AcqSetTriggerSrc(RP_TRIG_SRC_NOW) != RP_OK){ //Possible de mettre RP_TRIG_SRC_CHD_PE pour déclencher aqu sur le front montant de l'excitation
+        if( rp_AcqSetTriggerSrc(RP_TRIG_SRC_NOW) != RP_OK){
             fprintf(stderr, "rp_AcqSetTriggerSrc RP_TRIG_SRC_NOW failed!\n");
             return -1;
         }
@@ -259,16 +293,12 @@ int main(int argc, char **argv)
             }
         }
 
-       ////////////DECLENCHEMENT SYNCHRONISE///////////////
-        if(rp_GenSynchronise() != RP_OK){
-            fprintf(stderr, "rp_GenSynchronise failed!\n");
-            return -1;
-        }
+
          
-        printf ("wait to be filled\n");
+        //printf ("wait to be filled\n");
         while (!fillState) {
-            if (rp_AcqAxiGetBufferFillState(RP_CH_2, &fillState) != RP_OK) {
-                fprintf(stderr, "rp_AcqAxiGetBufferFillState RP_CH_2 failed!\n");
+            if (rp_AcqAxiGetBufferFillState(CH_ACQ, &fillState) != RP_OK) {
+                fprintf(stderr, "rp_AcqAxiGetBufferFillState CH_ACQ failed!\n");
                 return -1;
             }
         }
@@ -278,24 +308,64 @@ int main(int argc, char **argv)
                 return -1;
         }
 
-        if(rp_AcqAxiGetWritePointerAtTrig(RP_CH_2, &posChA)!=RP_OK){
+        if(rp_AcqAxiGetWritePointerAtTrig(CH_ACQ, &posChA)!=RP_OK){
             fprintf(stderr,"rp_AcqAxiGetWritePointerAtTrig Error");
         }
-        printf("Tr pos1: 0x%x\n",posChA);
+        //printf("Tr pos1: 0x%x\n",posChA);
 
-        if(rp_AcqAxiGetDataV(RP_CH_2, posChA, &dsize, buff1)!=RP_OK){
+        if(rp_AcqAxiGetDataV(CH_ACQ, posChA, &dsize, buff1)!=RP_OK){
             fprintf(stderr, "rp_AcqAxiGetDataV failed\n");
         }
         
-
         //////  Ecriture des données dans le fichier    //////
         printf("ecriture FID %d\n",i);
-        for (int i = 0; i < dsize; i++) {
+        for (int j = 0; j < dsize; j++) {
             //printf("[%d]\t%f\n",i,buff1[i]);
-            fprintf(fichier, "%f", buff1[i]);
-            if (i!= dsize -1) fprintf(fichier, ",");
+            fprintf(fichier, "%f", buff1[j]);
+            if (j!= dsize -1) fprintf(fichier, ",");
         }
         fprintf(fichier, "\n");
+    
+        /////////////////////////////////////////
+        ////// ----- Find max of Buff - ////////
+
+        maximum_amplitude_t[i+1] = find_max(buff1, dsize);
+        if (maximum_amplitude_t[i+1] != -1) {
+            printf("Max Amplitude : %f\n", maximum_amplitude_t[i+1]);
+        }
+
+        //////////////////////////////////////
+        ///// ---- MMPT ALGO ------ /////////
+        
+        if(maximum_amplitude_t[i+1]-maximum_amplitude_t[i]>0){
+            if(Larmor_frequency_Hertz_t[i+1] - Larmor_frequency_Hertz_t[i]>0){
+                Larmor_frequency_Hertz_t[i+2] = Larmor_frequency_Hertz_t[i+1] + step_frequency_Hertz;
+                printf("Augmentation de la fréquence\t");
+            }
+            else{
+                *(Larmor_frequency_Hertz_t+2) = *Larmor_frequency_Hertz_t - step_frequency_Hertz;
+                printf("Diminution de la fréquence\t");
+            }
+        }
+        
+        else{
+            if(Larmor_frequency_Hertz_t[i+1] - Larmor_frequency_Hertz_t[i]<0){
+                Larmor_frequency_Hertz_t[i+2] = Larmor_frequency_Hertz_t[i+1] - step_frequency_Hertz;
+                printf("Diminution de la fréquence\t");
+            }
+            else{
+                Larmor_frequency_Hertz_t[i+2] = Larmor_frequency_Hertz_t[i+1] + step_frequency_Hertz;
+                printf("Augmentation de la fréquence\t");
+ 
+            }
+        }
+        
+        printf("New Larmor Frequency = %f\n",Larmor_frequency_Hertz_t[i+2]);
+        
+        /// Recalcul des paramètres en fonction des nouvelles fréquences
+        excitation_burst_cycles_tot = Larmor_frequency_Hertz_t[i+2] * excitation_duration_seconds;
+        printf("New nombre des periodes dans le burst %d\n",excitation_burst_cycles_tot);
+
 
         usleep(wait);
 
@@ -307,10 +377,11 @@ int main(int argc, char **argv)
     printf("Temps d'execution : %lf\n",time_spent);
 
     fclose(fichier);
-
     /* Releasing resources */
-    rp_AcqAxiEnable(RP_CH_2, false);
+    rp_AcqAxiEnable(CH_ACQ, false);
     rp_Release();
     free(buff1);
+    free(Larmor_frequency_Hertz_t);
+    free(maximum_amplitude_t);
     return 0;
 }
